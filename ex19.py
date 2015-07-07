518d7eb3ade8b9bc9379141ba879e00e03c6f85c
BreenIsALie <m.jonsson2@gmail.com>
2015-07-07 22:03:44 +0200

    Finished exercise 19. More functions

---
+++ ex19.py
@@ -0,0 +1,25 @@
+__author__ = 'BreenIsALie'
+
+# Exercise 19, Exercises taken from http://learnpythonthehardway.org/book/
+# More functions and variables
+
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print "You have %d cheeses!" % cheese_count
+    print "You have %d boxes of crackers!" % boxes_of_crackers
+    print "That is a lot"
+    print "Get a blanket. \n"
+
+print "We can just give the function numbers directly:"
+cheese_and_crackers(20, 30)
+
+print "Or use variables from the script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print "We can do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+print "And we can combine the two, variables and math"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
