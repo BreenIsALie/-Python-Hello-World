12ef3125338c1402485cefaf09eb4b19a71d47c4
BreenIsALie <m.jonsson2@gmail.com>
2015-07-07 17:03:44 +0200

    Initial code. Simple print functions and basic math

---
+++ source.py
@@ -0,0 +1,34 @@
+__author__ = 'Sickan'
+
+# Initial Hello World environment test. Random basic code snippets abound below
+
+print "Hello World"
+print "Goodbye"
+
+# Math below
+print "Addition:", 20 + 30
+print "Subtraction:", 50 - 20
+print "Division:", 50 / 2
+print "Multiplication", 5*5
+
+# Logical operators
+print "Is 3+2 bigger then 5-7?"
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2", 3+2
+print "What is 5 - 7", 5-7
+
+print "Below is code relating to 5 and -2"
+print "Is 5 greater then -2 ?", 5 > -2
+print "is 5 greater or equal to -2?", 5>=-2
+print "Is 5 less or equal to -2?", 5 <=-2
+
+# Tests using floating point
+
+print "Print 20 as a float number", 20.0
+print "print 30 as a float number", 30.0
+
+print "Add together", 20.0 + 30.0
+
+print "Now with more decimals"
+print 20.5 + 30.5
