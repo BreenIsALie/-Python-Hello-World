22f09bc8e1b4138f0fcefd570d08012d82b2e5b8
BreenIsALie <m.jonsson2@gmail.com>
2015-07-07 19:00:23 +0200

    Finished and debugged exercise 9

---
+++ ex9.py
@@ -0,0 +1,21 @@
+__author__ = 'BreenIsALie'
+
+# Exercise 9, Exercises taken from http://learnpythonthehardway.org/book/
+# More print exercises
+
+days = "Mon Tue Wed Thu Fri Sat Sun" # initiate variable with string value
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # initiate variable with string value. Print each month on a new line ( \n )
+
+# print out each variable
+print "Here are the days:", days
+print "Here are the months", months
+
+# print a really long string using 3 double quotes
+print """
+Lots of stuff go here
+more stuff here
+3 lines total
+lets go for 4 lines
+the end
+"""
+
